875f21bc3f8a2755ec499ec4748afbbea21d1091
breakthatbass <gamache.taylor@gmail.com>
2021-04-29 13:26:22 -0400

    update rle api

--- rle.h
+++ rle.h
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 #include <string.h>
+#include <ctype.h>
 
 struct rle_file {
     int size;
@@ -16,5 +17,15 @@ struct rle_file {
 * user must free the pattern string (f.rle) after
 */
 void rle_parse(char *s, struct rle_file *f);
+
+
+/*
+ * load_grid:
+ *
+ * takes the grid and the entire pattern string containing multiple lines
+ * it breaks the string into tokens based on '$', decompresses it, and loads it
+ * into the grid.
+ */
+void load_grid(int **g, char *pattern)
 
 #endif
